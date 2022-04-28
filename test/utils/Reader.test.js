a79551639d9c5a6fa8e9df9e867978ba321aa0dd
felix <josevelazco134@gmail.com>
2022-04-28 00:11:12 -0600

    eslint fix command

--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -1,10 +1,10 @@
 const Reader = require("./../../lib/utils/Reader");
 
-describe('Reader test suite',() =>{
-  test('1) Read a file', () => {
+describe("Reader test suite",() =>{
+  test("1) Read a file", () => {
     const students = Reader.readJsonFile("data/students.json");
     expect(students).not.toBeNull();
     expect(students).not.toBeUndefined();
     expect(students.length).toBeGreaterThan(0);
-  })
-})
+  });
+});
