39a08670b3d116b47586bb136c33ee16ffea2320
felix <josevelazco134@gmail.com>
2022-04-28 01:21:35 -0600

    Change describe name

--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -3,7 +3,7 @@ const StudentService = require("./../../lib/services/StudentService");
 
 const students = Reader.readJsonFile("data/students.json");
 
-describe("Unit test suite",() =>{
+describe("StudentService test suite",() =>{
   test("1) Filter students with certification", () => {
     const studentsList = StudentService.filterStudentsCertification(students);
     const allCertification = studentsList.every((student) => student.haveCertification == true);
